89cd20640a17fce3dae4680078edcf14cffda852
SamuSan <sambminns@gmail.com>
2014-12-02 07:51:28 +1300

    Image for button

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,3 +1,7 @@
 $(function() {
-
+  $('.note_button').on('click', function(e){
+    console.log(e.target);
+    e.target.src = "app/assets/images/Button_on.png";
+    console.log(e.target);
+  });
 });
