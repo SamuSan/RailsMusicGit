a939dae8c7520763eed3cc06e2911e4d197a47e9
SamuSan <sambminns@gmail.com>
2014-12-08 21:25:55 +1300

    ploody fing is working now, plays audio, wierd bug with save now

--- app/assets/javascripts/drumMachine.js
+++ app/assets/javascripts/drumMachine.js
@@ -8,7 +8,7 @@ $(function(){
   console.log(tempo);
   beat = 60 / tempo;
   console.log(beat);
-  semi = beat / 16.0;
+  semi = beat / 4;
 });
 
 var DrumMachine = function() {
@@ -31,47 +31,17 @@ DrumMachine.prototype.play = function(triggerArrays) {
   // We'll start playing the rhythm 100 milliseconds from "now"
   var startTime = context.currentTime + 0.100;
   // var tempo = 80; // BPM (beats per minute)
-  var eighthNoteTime = (60 / tempo) / 2;
+  // var eighthNoteTime = (60 / tempo) / 2;
 
   // Play 2 bars of the following:
   for (var bar = 0; bar < 4; bar++) {
-console.log("INIT");
+    var time = startTime + bar * 16 * semi;
 
-    var time = startTime + bar * 8 * semi;
-
-    for(var click = 0; click < 16; click++){
-      triggerArrays.forEach(function(triggerArray) { 
-        triggerArray.forEach(function(trigger){
-          console.log(trigger)
-          if(trigger.position == click){
-            console.log("PLaying")
-            playSound(audioBuffers[triggerArrays.indexOf(triggerArray)]) 
-          }
-        })
+    triggerArrays.forEach(function(triggerArray) { 
+      triggerArray.forEach(function(trigger){
+        playSound(audioBuffers[triggerArrays.indexOf(triggerArray)], time + ((trigger.position % 16) * semi) )
       });
-    }
-    // triggerArrays.forEach(function(triggerArray) { 
-    //   triggerArray.forEach(function(trigger){
-    //     console.log(audioBuffers[triggerArrays.indexOf(triggerArray)])
-    //     console.log("THIS IS THE INDED " + triggerArrays.indexOf(triggerArray))
-    //     console.log("This is the position value "+trigger.position % 16);
-    //     playSound(audioBuffers[triggerArrays.indexOf(triggerArray)], time + ((trigger.position % 16) * semi) )
-
-
-    
-    // var time = startTime + bar * 8 * eighthNoteTime;
-    // // Play the bass (kick) drum on beats 1, 5
-    // playSound(this.kick, time);
-    // playSound(this.kick, time + 4 * eighthNoteTime);
-
-    // // Play the snare drum on beats 3, 7
-    // playSound(this.snare, time + 2 * eighthNoteTime);
-    // playSound(this.snare, time + 6 * eighthNoteTime);
-
-    // // Play the hi-hat every eighthh note.
-    // for (var i = 0; i < 8; ++i) {
-    //   playSound(this.hihat, time + i * eighthNoteTime);
-    // }
+    });  
   }
 };
 
@@ -79,7 +49,6 @@ function playSound(buffer, time) {
   var source = context.createBufferSource();
   source.buffer = buffer;
   source.connect(context.destination);
-  source.start();
-  // source[source.start ? 'start' : 'noteOn'](time);
+  source[source.start ? 'start' : 'noteOn'](time);
 }
 
--- spec/controllers/commits_controller_spec.rb
+++ spec/controllers/commits_controller_spec.rb
@@ -10,8 +10,28 @@ RSpec.describe CommitsController, type: :controller do
     context "when the comit is successfully created" do
       it "renders the commit histroy template" do
         xhr :post, :create, project_id: 1, parent_commit_id: 1, notes: notes
-        expect(response).to render_template("projects/_project_commit_history")
+        expect(response).to render_template("projects/_project_management")
       end
     end
+
+    context 'when ' do
+      
+    end
+
+
+    context "when there are notes for a commit" do
+      let(:project)     { projects(:with_title) }
+      let(:commit_one)  { commits(:commit_one)}
+      let(:notes)       { double }
+      
+      before do 
+        # expect(commit_one).to receive(:notes).and_return notes
+      end 
+
+      it "returns the notes in json format" do
+        xhr :get, :show, project_id:1, id:1, commit_number: 1
+        expect(response.status).to eq 200
+      end      
+    end 
   end
 end
